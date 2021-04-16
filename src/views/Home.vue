117ba7604573bd22cd77e020778f8727255d30cc
tuanama13 <tuanama7@gmail.com>
2021-04-16 16:50:31 +0700

    Add Button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div>
+    <input type="file" @change="onFileChange" class="px-8 py-3" >
+    <button class="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">Upload</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  components: {
-  }
+  data() {
+    return {
+      file:null
+    }
+  },
+  methods: {
+    onFileChange(e){
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(files[0]);
+    }
+  },
 }
 </script>
